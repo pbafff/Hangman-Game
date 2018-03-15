de1b2305f951784944b655cec0ecb9fd01075a1b
pbafff <andre.myers99@gmail.com>
2018-03-15 03:20:12 -0400

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var words = ["nauru", "kyrgyzstan", "brunei", "kiribati", "djibouti", "malta", "
 var wordsAndLatLng = {
     nauru: {
         latLng: { lat: -0.5284144, lng: 166.9342384 },
-        zoom: 7
+        zoom: 10
     },
     kyrgyzstan: {
         latLng: { lat: 41.20438, lng: 74.76610 },
@@ -15,7 +15,7 @@ var wordsAndLatLng = {
     },
     kiribati: {
         latLng: { lat: 1.826555, lng: -157.346610 },
-        zoom: 6
+        zoom: 9
     },
     djibouti: {
         latLng: { lat: 11.82514, lng: 42.59028 },
@@ -55,86 +55,90 @@ var lives = 12;
 document.getElementById("remainingGuesses").innerHTML = lives;
 
 document.onkeypress = function playerGuess(event) {
-    function getIndicesOf(searchStr, str) {
-        var searchStrLen = searchStr.length;
-        if (searchStrLen == 0) {
-            return [];
-        }
-        var startIndex = 0, index, indices = [];
-        while ((index = str.indexOf(searchStr, startIndex)) > -1) {
-            indices.push(index);
-            startIndex = index + searchStrLen;
-        }
-        return indices;
-    }
+    var k = event.key;
+    var alphabet = "qwertyuiopasdfghjklzxcvbnm";
+    if (alphabet.indexOf(k.toLowerCase()) !== -1) {
 
-    String.prototype.replaceAt = function (index, replacement) {
-        return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-    }
-
-    var keyPressed = event.key;
-    console.log(keyPressed);
-
-    if (randWord.indexOf(keyPressed) !== -1) {
-        var indices = getIndicesOf(keyPressed, randWord);
-        console.log(indices);
-        //loop through indices, place each value in hiddenWord[]
-        indices.forEach(function (index) {
-            console.log(index);
-            let hiddenWordUpdated = hiddenWord.replaceAt(index, keyPressed);
-            console.log(hiddenWord);
-            document.getElementById("demo").innerHTML = hiddenWordUpdated;
-            //strings are immutable
-            hiddenWord = hiddenWordUpdated;
-            if (hiddenWord === randWord) {
-                //change map location to hiddenWord
-
-                map.panTo(wordsAndLatLng[hiddenWord].latLng); //Make map global;
-                map.setZoom(wordsAndLatLng[hiddenWord].zoom);
-                marker.setPosition(wordsAndLatLng[hiddenWord].latLng);
-                marker.setAnimation(google.maps.Animation.DROP);
-
-                var j = words.indexOf(hiddenWord);
-                words.splice(j, 1);
-                console.log(words);
-                if (words === undefined || words.length == 0) {
-                    document.getElementById("winner").innerHTML = "You win!"
-                    return;
-                }
-                randWord = words[Math.floor(Math.random() * words.length)];
-                console.log(randWord + "~");
-                hiddenWord = "";
-                for (step = randWord.length; step > 0; step--) {
-                    hiddenWord += "_";
-                }
-                document.getElementById("demo").innerHTML = hiddenWord;
-
-                woops = "";
-                document.getElementById("wrongGuesses").innerHTML = "";
-
-                lives = 12;
-                document.getElementById("remainingGuesses").innerHTML = lives;
+        function getIndicesOf(searchStr, str) {
+            var searchStrLen = searchStr.length;
+            if (searchStrLen == 0) {
+                return [];
             }
-        });
-        document.getElementById("wrong").innerHTML = "";
-    }
-    else {
-        lives -= 1;
-        document.getElementById("remainingGuesses").innerHTML = lives;
-        if (lives < 1) {
-            document.getElementById("remainingGuesses").innerHTML = "You lose!";
-            return;
+            var startIndex = 0, index, indices = [];
+            while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+                indices.push(index);
+                startIndex = index + searchStrLen;
+            }
+            return indices;
         }
 
-        document.getElementById("wrong").innerHTML = "Guess again!";
+        String.prototype.replaceAt = function (index, replacement) {
+            return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+        }
 
-        if (woops.indexOf(keyPressed) == -1) {
-            var woopsUpdated = woops.concat(keyPressed);
-            document.getElementById("wrongGuesses").innerHTML = woopsUpdated;
-            woops = woopsUpdated;
+        var keyPressed = event.key.toLowerCase();
+        console.log(keyPressed);
+
+        if (randWord.indexOf(keyPressed) !== -1) {
+            var indices = getIndicesOf(keyPressed, randWord);
+            console.log(indices);
+            //loop through indices, place each value in hiddenWord[]
+            indices.forEach(function (index) {
+                console.log(index);
+                let hiddenWordUpdated = hiddenWord.replaceAt(index, keyPressed);
+                console.log(hiddenWord);
+                document.getElementById("demo").innerHTML = hiddenWordUpdated;
+                //strings are immutable
+                hiddenWord = hiddenWordUpdated;
+                if (hiddenWord === randWord) {
+                    //change map location to hiddenWord
+
+                    map.panTo(wordsAndLatLng[hiddenWord].latLng); //Make map global;
+                    map.setZoom(wordsAndLatLng[hiddenWord].zoom);
+                    marker.setPosition(wordsAndLatLng[hiddenWord].latLng);
+                    marker.setAnimation(google.maps.Animation.DROP);
+
+                    var j = words.indexOf(hiddenWord);
+                    words.splice(j, 1);
+                    console.log(words);
+                    if (words === undefined || words.length == 0) {
+                        document.getElementById("winner").innerHTML = "You win!"
+                        return;
+                    }
+                    randWord = words[Math.floor(Math.random() * words.length)];
+                    console.log(randWord + "~");
+                    hiddenWord = "";
+                    for (step = randWord.length; step > 0; step--) {
+                        hiddenWord += "_";
+                    }
+                    document.getElementById("demo").innerHTML = hiddenWord;
+
+                    woops = "";
+                    document.getElementById("wrongGuesses").innerHTML = "";
+
+                    lives = 12;
+                    document.getElementById("remainingGuesses").innerHTML = lives;
+                }
+            });
+            document.getElementById("wrong").innerHTML = "";
+        }
+        else {
+            lives -= 1;
+            document.getElementById("remainingGuesses").innerHTML = lives;
+            if (lives < 1) {
+                document.getElementById("remainingGuesses").innerHTML = "You lose!";
+                return;
+            }
+
+            document.getElementById("wrong").innerHTML = "Guess again!";
+
+            if (woops.indexOf(keyPressed) == -1) {
+                var woopsUpdated = woops.concat(keyPressed);
+                document.getElementById("wrongGuesses").innerHTML = woopsUpdated;
+                woops = woopsUpdated;
+            }
         }
     }
-
 }
 
 var myStyles = [
