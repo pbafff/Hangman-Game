c616c6afcc57514a3a52386494d30f13545c6dd4
pbafff <andre.myers99@gmail.com>
2018-03-17 18:20:16 -0400

    changed click to touchend

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -55,7 +55,7 @@ var lives = 12;
 document.getElementById("remainingGuesses").innerHTML = lives;
 
 document.getElementById("keyboardBtn").addEventListener("touchend", function( event ) {
-    document.getElementById("hiddenInput").focus({preventScroll:true});
+    document.getElementById("hiddenInput").focus();
   }, false);
 
 document.onkeyup = function playerGuess(event) {
